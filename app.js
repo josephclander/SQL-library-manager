fd56e56ec76cc52f5774f1a7f3bb18d874686d45
Joseph Lander <josephclander@gmail.com>
2021-05-25 17:13:01 +0100

    Fix: Issues of 404 render
    The tabs were not set correctly for pug to accept variable err

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   if (err.status === 404) {
-    res.render('page-not-found', { err: err.message });
+    res.render('page-not-found', { err });
   } else {
     res.render('error', { err });
   }
