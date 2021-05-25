b802f8498d30d48e4d0deb34a21e9713c3ae84e4
Joseph Lander <josephclander@gmail.com>
2021-05-25 12:09:07 +0100

    Add Book model connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const { Book } = require('../models');
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb) {
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (error) {
+      // Forward error to the global error handler
+      next(error);
+    }
+  };
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const books = await Book.findAll();
+    console.log(books);
+    res.json(books);
+  })
+);
 
 module.exports = router;
